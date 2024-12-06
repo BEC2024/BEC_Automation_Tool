046bbb0b1cf9662ccb9f038a25037117857f876b
Vimal Bhuriya <vimalb@monovative.com>
2024-12-06 18:04:07 +0530

    Remove the old code

--- BEC_Automation_Tool/SolidEdgeApp/SolidEdgeApp/SolidEdgeApp/BrookVille/Add_Update/VirtualStructure/VirtualAssemblyStructure/VirtualAssemblyClass.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class VirtualAssemblyClass
-
-    Public mainAssemblyName As String
-    Public dict1 As Dictionary(Of String, List(Of String)) = New Dictionary(Of String, List(Of String))
-    Public dicSubAssemblyDetails As Dictionary(Of String, VirtualAssemblyClass) = New Dictionary(Of String, VirtualAssemblyClass)()
-
-End Class
